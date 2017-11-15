4b00f492cb5ed7aa65198c74ce19dea51d25abc1
sudheerj <jsudheer50@gmail.com>
2017-11-15 10:03:49 +0800

    Checkbox components added

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # vue-ui
+[![Dependency Status][daviddm-image]][daviddm-url] ![Crates.io](https://img.shields.io/github/license/mashape/apistatus.svg)
 
 > Component library for VueJS framework
 
-## Build Setup
+## Install
 
-``` bash
-# install dependencies
+``` shell
 npm install --save vue-ui
+```
+
+##Quick start
+``` javascript
+import Vue from 'vue'
+import VueUI from 'vue-ui'
+
+Vue.use(VueUI)
+```
+
+### Components
+
+- Button
+- ProgressSpinner
+- ProgressBar
+- Checkbox
+- TriStateCheckbox
 
 # serve with hot reload at localhost:8080
 npm run dev
@@ -25,6 +42,10 @@ npm run e2e
 
 # run all tests
 npm test
-```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## LICENSE
+MIT
+
+[daviddm-image]: https://david-dm.org/sudheerj/vue-ui.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/sudheerj/vue-ui
---
+++ showcase/src/components/tristatecheckbox/tristatecheckboxdemo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+
+    <vueui-button @click="back" label="Back" style="left:0px"></vueui-button>
+    <div id="wrapper">
+      <h2>Features</h2>
+
+      <h3>1. Basic</h3>
+
+      <h3>Boolean - {{checked}}</h3>
+      <vueui-tristatecheckbox v-model="checked" binary="true"></vueui-tristatecheckbox>
+
+    </div>
+  </div>
+</template>
+<style lang="scss" src="../../../../src/assets/themes/omega/theme.scss"></style>
+<style lang="css" src="../../../../node_modules/font-awesome/css/font-awesome.min.css"></style>
+<script>
+
+  export default {
+    name: 'tristatecheckboxdemo',
+    data: function () {
+      return {
+          checked: false
+      }
+
+    },
+    methods: {
+
+      back: function () {
+        this.$router.go(-1);
+      }
+    }
+  };
+</script>
+<style>
+  #wrapper {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+
